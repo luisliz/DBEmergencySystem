1171d247f357247dcc68f07ac4bae53ce6ca7a14
Luis Liz <luis.liz@upr.edu>
2020-03-24 14:07:54 -0400

    added config file, DAO and updated users

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -1,27 +1,79 @@
 from flask import Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
 
 user_bp = Blueprint('user', __name__)
 
+users = {
+    1: {
+        'user_category_id': 1,
+        'first_name': 'Luis',
+        'last_name': 'Liz',
+        'dob': 938509285,
+        'email': 'luis@gmail.com',
+    },
+    2: {
+        'user_category_id': 1,
+        'first_name': 'Yeniel',
+        'last_name': 'Diaz',
+        'dob': 95002395,
+        'email': 'yeniel@gmail.com',
+    }
+}
+user_ranks = {
+    1: 'admin',
+    2: 'provider',
+    3: 'supplier',
+    4: 'user'
+}
 
 @user_bp.route('/')
 def index():
     return "Welcome to user"
 
-@user_bp.route('/user/profile/<int:user_id>', methods=['GET'])
-def get_profile(user_id): 
-    pass 
+@user_bp.route('/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    if(user_id in users):
+        return users[user_id]
+    return {'error': "User not found"}
 
-@user_bp.route('/login', methods=['GET', 'POST'])
-def login(): 
+@user_bp.route('/check_user_categories/<int:user_id>', methods=['GET'])
+def check_rank(user_id):
     pass
 
 @user_bp.route('/register', methods=['POST'])
-def register(): 
-    pass 
+def register():
+    password_hash = generate_password_hash(password)
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    # return render_template('login.html', title='Sign In', form=form)
+# @user_bp.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if(check_password_hash(self.password_hash, password)):
+#         return True
+#     else:
+#         return False
+#     pass
+
+@user_bp.route('/logout', methods=['GET', 'POST'])
+def login():
+    pass
+
 
 @user_bp.route('/activate', methods=['POST'])
 def activate():
-    pass 
+    pass
 
 @user_bp.route('/settings', methods=['GET', 'POST'])
 def settings():
