12e7a667199b4bfe9728e61ddc393f6bc89e48d2
Luis Liz <luis.liz@upr.edu>
2020-03-26 14:22:53 -0400

    Commented login out

--- app/blueprints/user.py
+++ app/blueprints/user.py
@@ -3,7 +3,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 user_bp = Blueprint('user', __name__)
 
-
 user_ranks = {
     1: 'admin',
     2: 'provider',
@@ -30,18 +29,18 @@ def register():
     password_hash = generate_password_hash(password)
     pass
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+#         login_user(user, remember=form.remember_me.data)
+#         return redirect(url_for('index'))
     # return render_template('login.html', title='Sign In', form=form)
 # @user_bp.route('/login', methods=['GET', 'POST'])
 # def login():
