081b88697e309e791ba622667e0d99aa30397027
YenielDiaz <yeniel.diaz@upr.edu>
2020-05-03 14:01:10 -0400

    Small Fixes for transaction table in schema file

--- app/schema.sql
+++ app/schema.sql
@@ -79,12 +79,12 @@ CREATE TABLE transactions (
 	pid SERIAL PRIMARY KEY,
 	tdate TIMESTAMP NOT NULL,
 	tquantity INTEGER NOT NULL,
-	uid INTEGER NOT NULL,
-	supplierUid INTEGER NOT NULL,
+	tpayerpid INTEGER NOT NULL,
+	tsupplierpid INTEGER NOT NULL,
 	rid INTEGER NOT NULL,
 	tamount FLOAT(10) NOT NULL,
-    FOREIGN KEY (uid) REFERENCES users(uid),
-    FOREIGN KEY (supplierUid) REFERENCES users(uid),
+    FOREIGN KEY (tpayerpid) REFERENCES payments(pid),
+    FOREIGN KEY (tsupplierpid) REFERENCES payments(pid),
     FOREIGN KEY (rid) REFERENCES resources(rid)
 );
 
