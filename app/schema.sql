c11ebd475e2a8ca57476a04668b5df761c5001c6
Jorge Vega <jorge.vega6@upr.edu>
2020-05-03 20:59:52 -0400

    fixed schema and added resource_by_request

--- app/schema.sql
+++ app/schema.sql
@@ -51,23 +51,24 @@ CREATE TABLE payments (
 
 CREATE TABLE requests (
     reqId SERIAL PRIMARY KEY,
-    reqQuantity INTEGER NOT NULL,
+    --reqQuantity INTEGER NOT NULL,
     reqPostDate TIMESTAMP NOT NULL,
     reqDispatchDate TIMESTAMP NOT NULL,
     reqLocation VARCHAR(50),
     requestUid INTEGER NOT NULL,
     supplierUid INTEGER,
-    rid INTEGER,
-    FOREIGN KEY (rid) REFERENCES resources(rid),
+    --rid INTEGER,
+    --FOREIGN KEY (rid) REFERENCES resources(rid),
     FOREIGN KEY (supplierUid) REFERENCES users(uid),
     FOREIGN KEY (requestUid) REFERENCES users(uid)
 );
 
-CREATE TABLE resources_by_request (
-    rrid SERIAL PRIMARY KEY,
-    reqid INTEGER,
-    rid INTEGER,
-    FOREIGN KEY (reqid) REFERENCES requests(reqId),
+CREATE TABLE resources_per_request (
+    rprId SERIAL PRIMARY KEY,
+    reqId INTEGER NOT NULL,
+    rid INTEGER NOT NULL,
+    reqQuantity INTEGER NOT NULL,
+    FOREIGN KEY (reqId) REFERENCES requests(reqId),
     FOREIGN KEY (rid) REFERENCES resources(rid)
 );
 
@@ -95,45 +96,39 @@ CREATE TABLE transactions (
     FOREIGN KEY (rid) REFERENCES resources(rid)
 );
 
-CREATE TABLE resource_by_transaction (
-    rtid SERIAL PRIMARY KEY,
-    tid INTEGER,
-    rid INTEGER,
-    FOREIGN KEY (tid) REFERENCES transactions(tid),
-    FOREIGN KEY (rid) REFERENCES resources(rid)
-);
 -------------------------------------------------------------------------
-----------------------------RESOURCES TABLES-----------------------------
+----------------------------RESOURCE TABLES------------------------------
 -------------------------------------------------------------------------
 
 CREATE TABLE medications (
     mid SERIAL PRIMARY KEY,
+    mname VARCHAR(50) NOT NULL,
     mmanufacturer VARCHAR(50) NOT NULL,
-    msize VARCHAR(50) NOT NULL,
+    msize VARCHAR(50) NOT NULL
  );
 
 CREATE TABLE canned_foods (
     canid SERIAL PRIMARY KEY,
     canbrand VARCHAR(50) NOT NULL,
-    cantype VARCHAR(50) NOT NULL,
+    cantype VARCHAR(50) NOT NULL
  );
 
 CREATE TABLE baby_foods (
     bid SERIAL PRIMARY KEY,
     bflavor VARCHAR(50) NOT NULL,
-    bbrand VARCHAR(50) NOT NULL,
+    bbrand VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE waters (
     wid SERIAL PRIMARY KEY,
     wcontainertype VARCHAR(50) NOT NULL,
-    wcontainersize VARCHAR(50) NOT NULL,
+    wcontainersize VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE dry_foods (
     dryid SERIAL PRIMARY KEY,
     drybrand VARCHAR(50) NOT NULL,
-    drytype VARCHAR(50) NOT NULL,
+    drytype VARCHAR(50) NOT NULL
 
 );
 
@@ -141,62 +136,63 @@ CREATE TABLE fuels (
     fid SERIAL PRIMARY KEY,
     fbrand VARCHAR(50) NOT NULL,
     ftype VARCHAR(50) NOT NULL,
-    fvolume VARCHAR(50) NOT NULL,
+    fvolume VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE heavy_equipments (
     hid SERIAL PRIMARY KEY,
     hbrand VARCHAR(50) NOT NULL,
-    htype VARCHAR(50) NOT NULL,
+    htype VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE clothings (
     clothid SERIAL PRIMARY KEY,
     clothbranch VARCHAR(50) NOT NULL,
     clothmaterial VARCHAR(50) NOT NULL,
-    clothtype VARCHAR(50) NOT NULL,
+    clothtype VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE power_generators (
     genid SERIAL PRIMARY KEY,
     genbrand VARCHAR(50) NOT NULL,
     gentype VARCHAR(50) NOT NULL,
-    genpower VARCHAR(50) NOT NULL,
+    genpower VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE medical_devices (
     meddevid SERIAL PRIMARY KEY,
     meddevbrand VARCHAR(50) NOT NULL,
-    meddevtype VARCHAR(50) NOT NULL,
+    meddevtype VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE batteries (
     batid SERIAL PRIMARY KEY,
     battype VARCHAR(50) NOT NULL,
-    batsize VARCHAR(50) NOT NULL,
+    batsize VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE tools (
     toolid SERIAL PRIMARY KEY,
     toolbrand VARCHAR(50) NOT NULL,
     tooltype VARCHAR(50) NOT NULL,
-    toolsize VARCHAR(50) NOT NULL,
+    toolsize VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE ices (
     iid SERIAL PRIMARY KEY,
     ibrand VARCHAR(50) NOT NULL,
     ibagsize VARCHAR(50) NOT NULL,
-    iweight VARCHAR(50) NOT NULL,
+    iweight VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE resources (
     rid SERIAL PRIMARY KEY,
+    rname VARCHAR(50) NOT NULL,
     mid INTEGER,
     canid INTEGER,
     bid INTEGER,
-    did INTEGER,
+    dryid INTEGER,
     fid INTEGER,
     hid INTEGER,
     clothid INTEGER,
@@ -209,15 +205,15 @@ CREATE TABLE resources (
     FOREIGN KEY (mid) REFERENCES medications(mid),
     FOREIGN KEY (canid) REFERENCES canned_foods(canid),
     FOREIGN KEY (bid) REFERENCES baby_foods(bid),
-    FOREIGN KEY (did) REFERENCES waters(did),
-    FOREIGN KEY (fid) REFERENCES dry_foods(fid),
-    FOREIGN KEY (hid) REFERENCES fuels(hid),
-    FOREIGN KEY (clothid) REFERENCES heavy_equipments(clothid),
+    FOREIGN KEY (dryid) REFERENCES dry_foods(dryid),
+    FOREIGN KEY (fid) REFERENCES fuels(fid),
+    FOREIGN KEY (hid) REFERENCES heavy_equipments(hid),
+    FOREIGN KEY (clothid) REFERENCES clothings(clothid),
     FOREIGN KEY (genid) REFERENCES power_generators(genid),
     FOREIGN KEY (meddevid) REFERENCES medical_devices(meddevid),
     FOREIGN KEY (batid) REFERENCES batteries(batid),
     FOREIGN KEY (toolid) REFERENCES tools(toolid),
     FOREIGN KEY (iid) REFERENCES ices(iid),
-    FOREIGN KEY (wid) REFERENCES waters(wid),
+    FOREIGN KEY (wid) REFERENCES waters(wid)
 );
 
